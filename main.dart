0e6a3bcaf2044d3334b3712118dd6715c74cb7fc
Muhammad Ali Mughal <jawantechpk@gmail.com>
2025-07-12 15:52:22 +0500

    learn conditional statement

---
+++ main.dart
@@ -0,0 +1,26 @@
+void main() {
+  print("Hello World");
+  int totalAssignment = 5;
+  int minAssignmentDone = 4;
+  int assignmentDone = 5;
+  bool islastClassAttended = true;
+  String sdtName = "Shahzeb";
+
+  // bool isAssignmentDone =true;
+  if (assignmentDone >= minAssignmentDone) {
+    print("Enter in Class");
+    bool isAssignmentCorrect = false;
+    if (isAssignmentCorrect) {
+      print("keep sitting");
+    } else {
+      print("Class se bahar");
+    }
+
+    sdtName = "abdullah";
+  } else if (islastClassAttended) {
+    print("Assignment should be done before 12");
+  } else {
+    print("Class se bahar");
+  }
+  print(sdtName);
+}
